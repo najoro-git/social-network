c28f74eba87d61cbdbe49d570ec0babca91adf0f
najoro-git <najororaian@gmail.com>
2026-04-05 09:26:11 +0300

    fix: disable healtscheck + wait for php-fpm

--- docker/start.sh
+++ docker/start.sh
@@ -6,5 +6,8 @@ echo "clear_env = no" >> /usr/local/etc/php-fpm.d/www.conf
 # Démarrer PHP-FPM en arrière-plan
 php-fpm -D
 
+# Attendre que PHP-FPM soit prêt
+sleep 2
+
 # Démarrer Nginx au premier plan
 nginx -g "daemon off;"
