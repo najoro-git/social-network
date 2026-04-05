b3e53e0eff2351efe4ca12564005a1a854773fa1
najoro-git <najororaian@gmail.com>
2026-04-05 10:35:08 +0300

    fix:generate nginx config dynamically with PORT

--- docker/start.sh
+++ docker/start.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 
-# Utiliser le Port de railway (defaut 80)
-PORT=${PORT:-80}
-
-# Remplacer $PORT dans la configuration Nginx
-sed -i "s/\$PORT/$PORT/g" /etc/nginx/http.d/default.conf
+# PORT Railway (défaut 80)
+export PORT=${PORT:-80}
 
 # Passer les variables d'env à PHP-FPM
 echo "clear_env = no" >> /usr/local/etc/php-fpm.d/www.conf
@@ -14,6 +11,33 @@ php-fpm -D
 
 # Attendre que PHP-FPM soit prêt
 sleep 2
+
+# Créer la config Nginx avec le bon port
+cat > /etc/nginx/http.d/default.conf << EOF
+server {
+    listen ${PORT};
+    server_name _;
+    root /var/www/html/public;
+    index index.php;
+
+    location / {
+        try_files \$uri \$uri/ /index.php?\$query_string;
+    }
+
+    location ~ \.php\$ {
+        fastcgi_pass 127.0.0.1:9000;
+        fastcgi_index index.php;
+        fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
+        include fastcgi_params;
+    }
+
+    location ~ /\.ht {
+        deny all;
+    }
+
+    client_max_body_size 10M;
+}
+EOF
 
 # Démarrer Nginx au premier plan
 nginx -g "daemon off;"
