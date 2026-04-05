f3a757ea7dd9626c8e884e9db68e67021c2b2943
najoro-git <najororaian@gmail.com>
2026-04-05 10:50:24 +0300

    fix:ngnix port via sed placeholder

--- docker/start.sh
+++ docker/start.sh
@@ -1,43 +1,22 @@
 #!/bin/sh
 
 # PORT Railway (défaut 80)
-export PORT=${PORT:-80}
+PORT=${PORT:-80}
 
 # Passer les variables d'env à PHP-FPM
 echo "clear_env = no" >> /usr/local/etc/php-fpm.d/www.conf
 
+# Remplacer le port dans la config nginx
+sed -i "s/NGINX_PORT/$PORT/" /etc/nginx/http.d/default.conf
+
 # Démarrer PHP-FPM en arrière-plan
 php-fpm -D
 
-# Attendre que PHP-FPM soit prêt
+# Attendre PHP-FPM
 sleep 2
 
-# Créer la config Nginx avec le bon port
-cat > /etc/nginx/http.d/default.conf << EOF
-server {
-    listen ${PORT};
-    server_name _;
-    root /var/www/html/public;
-    index index.php;
+# Tester la config nginx
+nginx -t
 
-    location / {
-        try_files \$uri \$uri/ /index.php?\$query_string;
-    }
-
-    location ~ \.php\$ {
-        fastcgi_pass 127.0.0.1:9000;
-        fastcgi_index index.php;
-        fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
-        include fastcgi_params;
-    }
-
-    location ~ /\.ht {
-        deny all;
-    }
-
-    client_max_body_size 10M;
-}
-EOF
-
-# Démarrer Nginx au premier plan
-nginx -g "daemon off;"
+# Démarrer Nginx
+exec nginx -g "daemon off;"
